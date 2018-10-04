359b673c0878ce7f389d7505436ef9678fe5043f
Patrick Harris <patricknpharris@gmail.com>
2018-10-04 10:48:01 +0100

    all specifications completed for first challenge

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -35,6 +35,10 @@ describe('Thermostat', function() {
   });
   
   describe('this.powerSavingMode', function() {
+    it('is true by default', function () {
+      expect(thermostat.powerSavingMode).toEqual(true);
+    });
+
     it('when on it should limit max temperature', function() {
       let maxTemp = POWERSAVING_ON_MAX_TEMP - DEFAULT_TEMP;
 
@@ -45,25 +49,52 @@ describe('Thermostat', function() {
     }); 
 
     it('when off it should limit max temperature to a different max temp', function () {
-      thermostat.powerSavingMode = false
-      let maxTemp = POWERSAVING_OFF_MAX_TEMP - DEFAULT_TEMP
+      thermostat.powerSavingMode = false;
+      let maxTemp = POWERSAVING_OFF_MAX_TEMP - DEFAULT_TEMP;
 
       for (let i = 0; i < maxTemp; i++) {
         thermostat.up();
       }
       expect(function() { thermostat.up() }).toThrowError('Maximum temperature reached!');
     }); 
-
-    it('is true by default', function() {
-      expect(thermostat.powerSavingMode).toEqual(true)
-    });
   });
 
   describe('#reset', function() {
     it('resets the temperature to the default temp', function() {
       thermostat.up();
       thermostat.reset();
-      expect(thermostat.temperature).toEqual(DEFAULT_TEMP)
+      expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
+    });
+  });
+
+  describe('#energyUsage', function() {
+    it('confirms low energy usage below low-usage temperature', function () {
+      let lowTemp = DEFAULT_TEMP - LOW_USAGE_TEMP;
+
+      for (let i = 0; i < lowTemp; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+
+    it('confirms medium usage between low and medium usage temps', function() {
+      let medTemp = DEFAULT_TEMP - MEDIUM_USAGE_TEMP;
+
+      for (let i = 0; i < medTemp; i++) {
+        thermostat.down();
+      };
+
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+
+    it('confirms high usage above medium usage temp', function () {
+      let maxTemp = POWERSAVING_ON_MAX_TEMP - DEFAULT_TEMP;
+
+      for (let i = 0; i < maxTemp; i++) {
+        thermostat.up();
+      };
+
+      expect(thermostat.energyUsage()).toEqual('high-usage');
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,8 @@ const MINIMUM_TEMP = 10
 const DEFAULT_TEMP = 20
 const POWERSAVING_ON_MAX_TEMP = 25
 const POWERSAVING_OFF_MAX_TEMP = 32;
+const LOW_USAGE_TEMP = 17
+const MEDIUM_USAGE_TEMP = 24 
 
 
 function Thermostat() {
@@ -18,7 +20,7 @@ Thermostat.prototype.up = function() {
 };
 
 Thermostat.prototype.down = function() {
-  if (this.temperature === MINIMUM_TEMP) {
+  if (this._isBelow(MINIMUM_TEMP)) {
     throw new Error("Minimum temperature reached!");
   }
     this.temperature--
@@ -28,7 +30,20 @@ Thermostat.prototype.reset = function () {
   this.temperature = DEFAULT_TEMP
 }
 
+Thermostat.prototype.energyUsage = function () {
+  if (this._isBelow(LOW_USAGE_TEMP)) {
+    return "low-usage";
+  } else if (this._isBelow(MEDIUM_USAGE_TEMP)) {
+    return "medium-usage";
+  } else {
+    return "high-usage";
+  }
+};
+
 Thermostat.prototype._isTemperatureReached = function() {
   return ((this.powerSavingMode && this.temperature >= POWERSAVING_ON_MAX_TEMP) || (!this.powerSavingMode && this.temperature >= POWERSAVING_OFF_MAX_TEMP))
 };
 
+Thermostat.prototype._isBelow = function(temperature) {
+  return this.temperature <= temperature;
+};
